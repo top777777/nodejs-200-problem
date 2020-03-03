492072fc0cfb4c7683beb4c12aa8d32f2f7a1f41
SANG BEOM KIM <top777777@gmail.com>
2020-03-03 20:15:44 +0900

    2020-03-03 studied

---
+++ 009.js
@@ -0,0 +1,7 @@
+let fruit = "apple";
+fruit1 = "banana";
+const pi = 3.14;
+pi = 3.15;
+
+console.log("fruit1 :", fruit1);
+console.log("pi :", pi);
---
+++ 011.js
@@ -0,0 +1,9 @@
+const isNaN123 = isNaN(123);
+const isNaNMinus123 = isNaN("-123");
+const isNaN234 = isNaN("234");
+const isNaNHello = "234";
+
+console.log(isNaN123);
+console.log(isNaNMinus123);
+console.log(isNaN234);
+console.log(isNaNHello);
